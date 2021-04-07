a03e4ef72e5bb5bfc4676e2796d70cd5c47924b7
karanveer <karan_4496@hotmail.com>
2021-04-07 13:41:54 -0400

    Send script now writes user specified messages

--- send.py
+++ send.py
@@ -1,18 +1,28 @@
 """
-Sends a single message to the queue and exits.
+Sends messages entered by the user to the queue.
 """
 
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+def main():
+    
+    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 
-channel = connection.channel()
+    channel = connection.channel()
 
-channel.queue_declare("hello")
+    channel.queue_declare("hello")
 
-channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+    try:
+        while True:
+            print()
+            message = input("> ")
+            channel.basic_publish(exchange="", routing_key="hello", body=message)
+            print(f"Sent '{message}'")
+    except KeyboardInterrupt:
+        print("Exiting")
+        connection.close()
+        exit(0)
 
-print("Sent 'Hello World!'")
 
-
-connection.close()  # flushes network buffers and gently closes connection.
+if __name__ == "__main__":
+    main()
